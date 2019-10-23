24b85338bbb1681082af7ff7f4f8ff4a50734e6a
Bjørnar Alvestad <bjornar.walle.alvestad@gmail.com>
2019-10-23 23:04:48 +0200

    parent selection

--- Eval.Core/Selection/Parent/TournamentParentSelection.cs
+++ Eval.Core/Selection/Parent/TournamentParentSelection.cs
@@ -12,7 +12,6 @@ namespace Eval.Core.Selection.Parent
         private readonly int _tournamentSize;
         private readonly double _prob;
 
-
         public TournamentParentSelection(IEAConfiguration configuration)
         {
             if (configuration.TournamentSize < 2)
--- Eval.Test.Unit/Selection/Parent/ParentSelectionTests.cs
+++ Eval.Test.Unit/Selection/Parent/ParentSelectionTests.cs
@@ -8,6 +8,7 @@ using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Eval.Core.Util;
+using System.Diagnostics;
 
 namespace Eval.Test.Unit.Selection.Parent
 {
@@ -16,6 +17,7 @@ namespace Eval.Test.Unit.Selection.Parent
         private readonly double _mockedFitness;
         public int Index { get; }
         public int Count { get; set; }
+        public int Count2 { get; set; }
 
         public TestPhenotype(int index, double mockedFitness)
             : base(new Mock<IGenotype>().Object)
@@ -136,34 +138,93 @@ namespace Eval.Test.Unit.Selection.Parent
             }
         }
 
-        /*
         [TestMethod]
         public void TournamentParentSelection_VerifySelectionProbability()
         {
-            var population = new Population(11);
-            var index = 0;
-            population.Fill(() => new TestPhenotype(index++, index * index));
-            population.Sort(EAMode.MaximizeFitness);
+            // Test tournament selection by comparing it to the old "established" tournament selection implementation.
 
-            var selection = new TournamentParentSelection();
-            var selected = selection.SelectParents(population, 275000, EAMode.MaximizeFitness, random);
-
-            foreach (var (a, b) in selected)
+            for (int tournamentSize = 2; tournamentSize <= 10; tournamentSize++)
             {
-                ((TestPhenotype)a).Count++;
-                ((TestPhenotype)b).Count++;
+                var population = new Population(11);
+                var index = 0;
+                population.Fill(() => new TestPhenotype(index++, index * index));
+                population.Sort(EAMode.MaximizeFitness);
+
+                var config = new EAConfiguration
+                {
+                    TournamentSize = 3,
+                    TournamentProbability = 0.5
+                };
+
+                var oldselection = new TournamentParentSelectionOld(config);
+                var oldselected = oldselection.SelectParents(population, 275000, EAMode.MaximizeFitness, random);
+
+                foreach (var (a, b) in oldselected)
+                {
+                    ((TestPhenotype)a).Count2++;
+                    ((TestPhenotype)b).Count2++;
+                }
+
+                var selection = new TournamentParentSelection(config);
+                var selected = selection.SelectParents(population, 275000, EAMode.MaximizeFitness, random);
+
+                foreach (var (a, b) in selected)
+                {
+                    ((TestPhenotype)a).Count++;
+                    ((TestPhenotype)b).Count++;
+                }
+
+                var counts = population.Cast<TestPhenotype>().Select(p => p.Count).Zip(population.Cast<TestPhenotype>().Select(p => p.Count2), (c1, c2) => (c1, c2));
+
+                foreach (var count in counts)
+                {
+                    (count.c1 - count.c2).Should().BeLessThan(1000, $"tournament size = {tournamentSize}");
+                }
             }
+        }
 
-            var std = population.Select(p => p.Fitness).StandardDeviation();
-            var avg = population.Select(p => p.Fitness).Average();
-            var psum = population.Sum(p => 1 + (p.Fitness - avg) / (2 * std));
+        [TestMethod, Ignore]
+        public void TournamentSelection_PerfTest()
+        {
+            var random = new DefaultRandomNumberGenerator();
 
-            foreach (TestPhenotype p in population)
+            var config = new EAConfiguration
             {
-                var prob = (1 + (p.Fitness - avg) / (2 * std)) / psum;
-                var expectedCount = prob * 275000 * 2;
-                p.Count.Should().BeCloseTo((int)expectedCount, 1000);
+                TournamentSize = 20,
+                TournamentProbability = 0.5
+            };
+
+            var popsize = 10000;
+            var pop = new Population(popsize);
+            pop.Fill(() => CreatePhenotypeMock(random.NextDouble() * 10).Object);
+            pop.Sort(EAMode.MaximizeFitness);
+
+            Console.WriteLine($"popsize = {popsize}, tournament size = {config.TournamentSize}");
+
+            var watch = new Stopwatch();
+            watch.Start();
+            IParentSelection selection = new TournamentParentSelectionOld(config);
+            foreach (var selected in selection.SelectParents(pop, popsize, EAMode.MaximizeFitness, random))
+            {
             }
-        }*/
+            watch.Stop();
+            Console.WriteLine($"old: {watch.ElapsedMilliseconds} ms");
+
+            watch.Restart();
+            selection = new TournamentParentSelection(config);
+            foreach (var selected in selection.SelectParents(pop, popsize, EAMode.MaximizeFitness, random))
+            {
+            }
+            watch.Stop();
+            Console.WriteLine($"new: {watch.ElapsedMilliseconds} ms");
+        }
+
+        private static Mock<IPhenotype> CreatePhenotypeMock(double fitnessSetup)
+        {
+            var pmock = new Mock<IPhenotype>();
+            pmock.SetupGet(p => p.Fitness).Returns(fitnessSetup);
+            pmock.SetupGet(p => p.IsEvaluated).Returns(true);
+            return pmock;
+        }
     }
 }
