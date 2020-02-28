54fb1a376160258a54df48efc9930e07ff5634fc
Bjørnar Walle Alvestad <bjornar.alvestad@lindbak.no>
2020-02-28 14:46:36 +0100

    Added larseendendr to license

--- Eval.Test.Unit/Models/ArrayGenotypeTests.cs
+++ Eval.Test.Unit/Models/ArrayGenotypeTests.cs
@@ -1,7 +1,7 @@
 ﻿#region LICENSE
 /* 
  * Distributed under the MIT License.
- * Copyright (c) 2020 Viktor Zoric, Bjørnar Walle Alvestad
+ * Copyright (c) 2019-2020 Viktor Zoric, Bjørnar Walle Alvestad, Endre Larsen
  * 
  * Read full license terms in the accompanying LICENSE file or at https://opensource.org/licenses/MIT
  */
